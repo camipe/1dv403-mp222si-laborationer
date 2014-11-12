8108b6d34f5486560e7af1e45418cd0457ff86d2
Micael Persson <micael.c.persson@gmail.com>
2014-11-12 16:57:48 +0100

    Result-object returns the names in a string now.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,33 @@
 
 var makePerson = function(persArr){
 
+  // Declaring variables
+  var result = {};
+  var ages = [];
+  var names = [];
+  var sortedNames = "";
 
-	// Din kod här...
+  // Returns an array of all the names form the array persArr
+  names = persArr.map(function(obj) {
+    return obj.name;
+  });
+  // Sorts the names alphabetically
+  sortedNames = names.sort(function(a,b){return a.localeCompare(b)});
 
-}
+  ages = persArr.map(function(obj) {
+    return obj.age;
+  });
+
+  // Add Names to result object as a string
+  result.names = sortedNames.join(", ");
+
+  return result;
+};
+
+
+ var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+ var result = makePerson(data);
+
+ console.log(result);
 
