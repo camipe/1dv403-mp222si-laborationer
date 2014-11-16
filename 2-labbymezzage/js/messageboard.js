81234b8fb3a52fa0cb3af8bc43a42d66d8a48c3f
Micael Persson <micael.c.persson@gmail.com>
2014-11-16 22:45:01 +0100

    Working on the HTML structure and styling. First draft.

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -2,19 +2,13 @@
 
 var Messageboard = {
 
+  messages: [],
+
   init: function() {
     var mess = new Message("Hejhejhej hoho", new Date());
-    console.log(mess);
-    console.log(mess.getText());
-    console.log(mess.getDate());
-    console.log(mess.toString());
-    mess.setText("Ny text, ny text");
-    mess.setDate(new Date("2012-11-12"));
-    console.log(mess.getText());
-    console.log(mess.getDate());
-    console.log(mess.toString());
   }
 
 };
 
 window.onload = Messageboard.init;
+
