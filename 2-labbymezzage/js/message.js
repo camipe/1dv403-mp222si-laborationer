6fb2faadcd967eca9ddd1a2e8751eab93a62b60a
Micael Persson <micael.c.persson@gmail.com>
2014-11-16 15:56:36 +0100

    Moment 2 complete.

--- 2-labbymezzage/js/message.js
+++ 2-labbymezzage/js/message.js
@@ -1,3 +1,24 @@
-function Message() {
+function Message(message, date) {
 
-};
+  // Get/Set text
+  this.getText = function(){
+    return message;
+  }
+  this.setText = function(_text) {
+    message = _text;
+  }
+
+  // Get/Set date
+  this.getDate = function(){
+    return date;
+  }
+  this.setDate = function(_date){
+    date = _date;
+  }
+}
+
+Message.prototype.toString = function() {
+  return this.getText() + " (" + this.getDate() + ") ";
+}
+
+Message.prototype.getHTMLText = "Not implemented yet";
--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -3,7 +3,16 @@
 var Messageboard = {
 
   init: function() {
-    console.log("Test");
+    var mess = new Message("Hejhejhej hoho", new Date());
+    console.log(mess);
+    console.log(mess.getText());
+    console.log(mess.getDate());
+    console.log(mess.toString());
+    mess.setText("Ny text, ny text");
+    mess.setDate(new Date("2012-11-12"));
+    console.log(mess.getText());
+    console.log(mess.getDate());
+    console.log(mess.toString());
   }
 
 };
