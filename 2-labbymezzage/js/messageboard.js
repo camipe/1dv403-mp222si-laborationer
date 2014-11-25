560d8f49d7dbc623c79e32da9f99adaf56b88924
Micael Persson <micael.c.persson@gmail.com>
2014-11-25 14:46:30 +0100

    newMessage function working.

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -1,14 +1,26 @@
 "use strict";
 
+var wrapper = document.querySelector(".wrapper");
+var input = document.querySelector("#textInput");
+var send = document.querySelector("#send");
+
 var Messageboard = {
 
   messages: [],
 
   init: function() {
-    var mess = new Message("Hejhejhej hoho", new Date());
+
+  },
+
+  newMessage: function() {
+    var mess = new Message(input.value, new Date());
+    Messageboard.messages.push(mess);
+    // console.log(Messageboard.messages.toString());
   }
 
 };
 
 window.onload = Messageboard.init;
+
+send.onclick = Messageboard.newMessage;
 
