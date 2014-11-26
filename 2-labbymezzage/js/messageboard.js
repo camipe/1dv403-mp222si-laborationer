d891e40871be7103e3e47e31393a3275381a4818
Micael Persson <micael.c.persson@gmail.com>
2014-11-26 22:44:15 +0100

    Added some comments.

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -41,6 +41,7 @@ var Messageboard = {
     };
   },
 
+  // Render single message
   renderMessage: function(messageID) {
     var div = document.createElement("div");
     var text = document.createElement("p");
@@ -50,36 +51,43 @@ var Messageboard = {
     var timeButton = document.createElement("a");
     var timeButtonImg = document.createElement("img");
 
+    // Create div to hold message
     div.className = "message";
     div.id = messageID;
     Messageboard.messageArea.appendChild(div);
-
+    // Insert text from input field to div
     text.innerHTML = Messageboard.messages[messageID].getHTMLText();
     div.appendChild(text);
 
+    // Insert button for time alert
     timeButtonImg.src = "img/circular191.png";
     timeButton.href = "#";
     timeButton.appendChild(timeButtonImg);
     div.appendChild(timeButton);
 
+    // Insert button deleting messages
     deleteMessageImg.src = "img/close33.png";
     deleteMessageButton.href = "#";
     deleteMessageButton.appendChild(deleteMessageImg);
     div.appendChild(deleteMessageButton);
 
+    // Insert timestamp
     timeText.innerHTML = Messageboard.messages[messageID].getDate().toLocaleTimeString();
     div.appendChild(timeText);
 
+    // Set onclick behaviour for buttons
     timeButton.onclick = Messageboard.timeAlert;
     deleteMessageButton.onclick = Messageboard.removeMessage;
   },
 
+  // Function to remove messages
   removeMessage: function() {
     Messageboard.messages.splice(this.parentNode.id, 1);
     Messageboard.renderMessages();
     return false;
   },
 
+  // Function to show an alert with the message creation time
   timeAlert: function() {
     var timeString = Messageboard.messages[this.parentNode.id].getDate().toLocaleString('se');
     alert("Inlägget skapdes " + timeString);
