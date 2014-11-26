3bcc57efc55b64f90adfd6b7e0ae1f43aa7858ef
Micael Persson <micael.c.persson@gmail.com>
2014-11-26 22:24:15 +0100

    Timealert when clicking on clock icon.

--- 2-labbymezzage/js/messageboard.js
+++ 2-labbymezzage/js/messageboard.js
@@ -38,8 +38,10 @@ var Messageboard = {
     var div = document.createElement("div");
     var text = document.createElement("p");
     var timeText = document.createElement("p");
-    var deleteMessage = document.createElement("a");
+    var deleteMessageButton = document.createElement("a");
     var deleteMessageImg = document.createElement("img");
+    var timeButton = document.createElement("a");
+    var timeButtonImg = document.createElement("img");
 
     div.className = "message";
     div.id = messageID;
@@ -48,21 +50,32 @@ var Messageboard = {
     text.innerHTML = Messageboard.messages[messageID].getHTMLText();
     div.appendChild(text);
 
-    deleteMessageImg.src = "img/close33.png"
-    deleteMessage.href = "#"
-    deleteMessage.appendChild(deleteMessageImg);
-    div.appendChild(deleteMessage);
+    timeButtonImg.src = "img/circular191.png";
+    timeButton.href = "#";
+    timeButton.appendChild(timeButtonImg);
+    div.appendChild(timeButton);
+
+    deleteMessageImg.src = "img/close33.png";
+    deleteMessageButton.href = "#";
+    deleteMessageButton.appendChild(deleteMessageImg);
+    div.appendChild(deleteMessageButton);
 
     timeText.innerHTML = Messageboard.messages[messageID].getDate().toLocaleTimeString();
     div.appendChild(timeText);
 
-    deleteMessage.onclick = Messageboard.removeMessage;
+    timeButton.onclick = Messageboard.timeAlert;
+    deleteMessageButton.onclick = Messageboard.removeMessage;
   },
 
   removeMessage: function() {
     Messageboard.messages.splice(this.parentNode.id, 1 );
     Messageboard.renderMessages();
+    return false;
+  },
 
+  timeAlert: function() {
+    var timeString = Messageboard.messages[this.parentNode.id].getDate().toLocaleString('se');
+    alert("Inlägget skapdes " + timeString);
   }
 
 };
