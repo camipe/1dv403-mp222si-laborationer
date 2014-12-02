7f6b02f5d42ae9df07a746b7802fcfb9ab854100
Micael Persson <micael.c.persson@gmail.com>
2014-12-02 11:02:45 +0100

    Added brickcounter to keep track of the number bricks on the board and gives them an id.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -2,8 +2,8 @@
 
 var Memory = {
 
-  gameRows: 3,
-  gameCols: 3,
+  gameRows: 2,
+  gameCols: 2,
 
   brickOrder: [],
 
@@ -16,8 +16,9 @@ var Memory = {
   // Info - https://developer.mozilla.org/en-US/docs/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
   generateBoard: function(array) {
     var board = document.getElementById("board");
-
     var table = document.createElement("table");
+
+    var brickCounter = 0;
 
     for (var i = 0; i < Memory.gameRows; i += 1) {
     // creates a table row
@@ -33,17 +34,25 @@ var Memory = {
 
         brickImg.src = "pics/0.png";
         brick.href = "#";
+        brick.id = brickCounter;
         brick.appendChild(brickImg);
         cell.appendChild(brick);
         row.appendChild(cell);
+
+        brickCounter += 1;
+        brick.addEventListener("click", Memory.turnBrick)
       }
 
       table.appendChild(row);
     }
 
     board.appendChild(table);
+  },
 
-
+  turnBrick: function(e) {
+    console.log("hej")
+    console.log(this)
+    console.log(this.parentNode)
   }
 };
 
