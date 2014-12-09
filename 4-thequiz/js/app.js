2959db1a7d4383cc0b8c7376e0ed930e94f17de1
Micael Persson <micael.c.persson@gmail.com>
2014-12-09 16:56:18 +0100

    Refactored code to get question to a separate function.

--- 4-thequiz/js/app.js
+++ 4-thequiz/js/app.js
@@ -1,29 +1,41 @@
 "use strict";
 
 var app = {
+
+  startURL: "http://vhost3.lnu.se:20080/question/1",
+
   init: function() {
     var startButton = document.getElementById("start");
-    startButton.addEventListener("click", function() {
-      var xhr = new XMLHttpRequest();
+    startButton.addEventListener("click", app.getQuestion.bind(null, app.startURL));
+
+    var answer = {"answer" : "2"};
+
+  },
+
+  getQuestion: function(url) {
+    var xhr = new XMLHttpRequest();
+
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         console.log(xhr.responseText);
       };
     };
-
-    xhr.open('GET', 'http://vhost3.lnu.se:20080/question/1', true);
+    xhr.open('GET', url, true);
     xhr.send();
+  },
 
-    var answer = {"answer" : "2"};
-    var xhr2 = new XMLHttpRequest();
+  sendAnswer: function() {
+    var xhr = new XMLHttpRequest();
 
-    xhr2.onreadystatechange = function(){console.log(xhr2.responseText)};
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState === 4) {
+        console.log(xhr.responseText);
+      };
+    };
+
     xhr2.open('POST', 'http://vhost3.lnu.se:20080/answer/1', true);
     xhr2.setRequestHeader('Content-Type', 'application/json');
     xhr2.send(JSON.stringify(answer));
-
-    })
-
   }
 
 };
@@ -33,8 +45,16 @@ window.onload = app.init;
 
 
 // En funktion som hämtar första frågan
-
-// En funktion hämtar nästa frågan
+  // Variabel som håller koll på start url
+  // Hämta första frågan
+  // Skriv ut på sidan
 
 // En funktion som skriver ut frågan
+  // Ta ut "question" från objektet
+  // Skapa HTML-element
+  // Lägg till elementen på sidan
+
+// Hämta fråga (URL)
+  // Skapa XHR-objekt
+  // onreadystatechange, lagra svarsobjektet i array
 
