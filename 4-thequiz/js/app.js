d4381ce90e5700cc72afeb271ef6d29a50a7695f
Micael Persson <micael.c.persson@gmail.com>
2014-12-11 10:29:01 +0100

    Added some comments and styling.

--- 4-thequiz/js/app.js
+++ 4-thequiz/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var app = {
-
+  // Variables
   startURL: "http://vhost3.lnu.se:20080/question/1",
   responses: [],
   tries: 0,
@@ -9,45 +9,59 @@ var app = {
   endOfGame: false,
 
   init: function() {
+    // Inititing onclick for the the start button
     var startButton = document.getElementById("start");
     startButton.addEventListener("click", app.getQuestion.bind(null, app.startURL));
 
   },
-
+  // Function that get the next question
   getQuestion: function(url) {
-    var xhr = new XMLHttpRequest();
+    // Only get question if EndOfGame is false
     if (!app.endOfGame) {
+
+      var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
+
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          // Save the response and print the page
           app.saveResponse(xhr.responseText, app.responses);
           app.printPage();
+
         };
       };
+
       xhr.open('GET', url, true);
       xhr.send();
 
-    } else {
-      app.printPage();
     }
   },
-
+  // Function to send answer
   sendAnswer: function(answer) {
-    var xhr = new XMLHttpRequest();
+
     var URL = app.responses[app.responses.length - 1].nextURL;
     var JSONAnswer = {"answer" : answer}
+
+    var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
+
       if (xhr.readyState === 4) {
+        // Add 1 to current question's counter
         app.tries += 1;
+
+        // if the answer is correct save the response, push counter to array
+        // and reset the current counter, then either print result or the next question
         if (xhr.status === 200) {
+
           app.saveResponse(xhr.responseText, app.responses);
           app.allTries.push(app.tries);
           app.tries = 0;
+
           if (app.endOfGame) {
             app.printResult();
           } else {
+
             app.getQuestion(app.responses[app.responses.length - 1].nextURL)
             app.printPage();
-
 
           }
         } else {
@@ -63,15 +77,16 @@ var app = {
   },
 
   printPage: function() {
+    // Variables
     var div = document.querySelector(".quiz");
     var text = document.createElement("p");
     var button = document.createElement("button");
     var input = document.createElement("input");
     var arrItem = app.responses[app.responses.length - 1];
-
+    // Clear the div
     div.innerHTML = "";
 
-
+    // Extract the question from the last saved server response
     text.innerHTML = app.responses[app.responses.length - 1].question;
     input.id = "textinput";
     button.innerHTML = "Skicka";
@@ -81,31 +96,39 @@ var app = {
       app.sendAnswer(input.value);
     });
 
+    // Append elements to DOM
+    div.appendChild(text);
     div.appendChild(input);
     div.appendChild(button);
-    div.appendChild(text);
 
   },
 
   printResult: function() {
+    // Variables
     var div = document.querySelector(".quiz");
     var text = document.createElement("p");
     var list = document.createElement("ol")
 
+    // Clear div
     div.innerHTML = "";
 
-    text.innerHTML = "Grattis, du har klarat alla frågor. Sen hur många försök det tog nedan.<br>" +
-    "Fråga:"
+    text.innerHTML = "Grattis, du har klarat alla frågor. " +
+    "Se hur många försök det tog nedan.<br>Fråga:"
+    // Loop through all the counters and print them in ordered list
     for (var i = 0; i < app.allTries.length; i+=1) {
+
       var li = document.createElement("li");
       var textnode = document.createTextNode(app.allTries[i] + " försök.");
       li.appendChild(textnode);
       list.appendChild(li);
+
     }
+
     div.appendChild(text);
     div.appendChild(list);
-  },
 
+  },
+  // Function which tells the user when the answer was wrong
   printError: function() {
     var div = document.querySelector(".quiz");
     var text = document.querySelector("#error") || document.createElement("p");
@@ -115,33 +138,20 @@ var app = {
     div.appendChild(text);
 
   },
-
+  // Function which saves the server response
   saveResponse: function(response, targetArr) {
     var responseObj = JSON.parse(response);
     var arr = targetArr;
+
     if (!responseObj.hasOwnProperty("nextURL")) {
       app.endOfGame = true;
     };
+
     arr.push(responseObj);
+
   },
 
 };
 
 window.onload = app.init;
-
-
-
-// En funktion som hämtar första frågan
-  // Variabel som håller koll på start url
-  // Hämta första frågan
-  // Skriv ut på sidan
-
-// En funktion som skriver ut frågan
-  // Ta ut "question" från objektet
-  // Skapa HTML-element
-  // Lägg till elementen på sidan
-
-// Hämta fråga (URL)
-  // Skapa XHR-objekt
-  // onreadystatechange, lagra svarsobjektet i array
 
