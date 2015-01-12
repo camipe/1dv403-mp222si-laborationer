1eceeec5cb90ef74146909d9aff59365889e6ccb
Micael Persson <micael.c.persson@gmail.com>
2015-01-12 14:19:32 +0100

    Working on basic structure.

--- 5-pwd/js/app.js
+++ 5-pwd/js/app.js
@@ -1,5 +1,22 @@
 "use strict";
 
-window.onload = function() {
-  console.log("Test")
-};
+// Klick på ikonen
+  // Skapa en "window"-div
+    // Stängningsknapp i hörnet
+      // När knappen klickas på så stängs fönstret
+  // Tolka JSON-obj
+  // Skapa bildgalleriet från informationen
+    // Om det tar tid ska en laddningsanimation visas
+
+var app = {
+
+  init: function() {
+    console.log("Fungerar")
+  }
+}
+
+
+
+window.onload = app.init();
+
+
