424d04d1a87d5931fa996260cd24af4588ed2158
Micael Persson <micael.c.persson@gmail.com>
2015-01-12 21:56:50 +0100

    Window can be opened and closed.

--- 5-pwd/js/app.js
+++ 5-pwd/js/app.js
@@ -10,27 +10,39 @@
 
 var app = {
 
-  images: [],
+  imageHeight: 0,
+  imageWidth: 0,
 
   init: function() {
-    console.log("Fungerar")
-    app.openWindow();
-    app.getImageInfo();
+
+    $( "#imgviewer" ).on("click", function (e){
+      e.preventDefault()
+      app.openWindow();
+    });
   },
 
   openWindow: function() {
     var $window = $('<div class="window"></div>');
     var $header = $('<header class="topbar"></header>');
     var $appDiv = $('<div class="appDiv"></div>');
+    var $closeButton = $('<a href="#" class="close-button">X</a>');
 
     $('<h2>ImageViewer</h2>').appendTo($header);
-    $('<a href="#" class="close-button">X</a>').appendTo($header);
+    $closeButton.appendTo($header);
 
     $header.appendTo($window);
     $appDiv.appendTo($window);
 
     $window.appendTo("#desktop");
+
+    app.getImageInfo();
+
+    $closeButton.click(function() {
+      $window.remove();
+    })
   },
+
+  closeWindow: function() {},
 
   getImageInfo: function() {
     var $data = $.ajax({
@@ -40,7 +52,7 @@ var app = {
         // console.log($data.responseText)
         var imgList = JSON.parse($data.responseText);
 
-        console.log("Success")
+        app.getImgRes(imgList);
         app.displayImages(imgList);
 
       },
@@ -50,18 +62,30 @@ var app = {
     })
   },
 
+  getImgRes: function(array) {
+
+    for (var i = 0; i < array.length; i++) {
+      if (app.imageHeight < array[i].thumbHeight) {
+        app.imageHeight = array[i].thumbHeight;
+      };
+      if (app.imageWidth < array[i].thumbWidth) {
+        app.imageWidth = array[i].thumbWidth;
+      };
+    };
+  },
+
   displayImages: function(array) {
     var imgInfo;
     var $link;
     var $img;
-    var $appDiv = $(".appDiv")
+    var $appDiv = $(".appDiv");
 
     for (var i = 0; i < array.length; i++) {
       imgInfo = array[i];
 
       $link = $('<a></a>');
       $link.attr("href", imgInfo.URL);
-      $link.addClass("thumbNail");
+      $link.addClass("thumbnail");
 
 
       $img = $('<img>')
@@ -72,6 +96,9 @@ var app = {
 
       $img.appendTo($link);
       $link.appendTo($appDiv);
+
+      $(".thumbnail").css("height", (app.imageHeight + 4))
+      $(".thumbnail").css("width", (app.imageWidth + 4))
     };
   }
 };
