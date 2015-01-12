a21b79c04c160ed1f023b2b4b95ce7001618e7bc
Micael Persson <micael.c.persson@gmail.com>
2015-01-12 15:48:36 +0100

    Can open window and get data from the server.

--- 5-pwd/js/app.js
+++ 5-pwd/js/app.js
@@ -12,13 +12,41 @@ var app = {
 
   init: function() {
     console.log("Fungerar")
+    app.openWindow();
+    app.getImages();
   },
 
   openWindow: function() {
-    var $window = $('<div class="window"></div>').appendTo("body");
-  }
-}
+    var $window = $('<div class="window"></div>');
+    var $header = $('<header class="topbar"></header>');
+    var $appDiv = $('<div class="appDiv"></div>');
 
+    $('<h2>ImageViewer</h2>').appendTo($header);
+    $('<a href="#" class="close-button">X</a>').appendTo($header);
+
+    $header.appendTo($window);
+    $appDiv.appendTo($window);
+
+    $window.appendTo("#desktop");
+  },
+
+  getImages: function() {
+    var $data = $.ajax({
+      url: 'http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/',
+      type: 'GET',
+      success: function() {
+        //called when successful
+        console.log("Success")
+        console.log($data.responseText)
+      },
+      error: function(e) {
+        console.log(e.message);
+      }
+    })
+
+
+  }
+};
 
 
 window.onload = app.init();
